47a8f31f1ce0dbd189ad2112cd2c99fd00a97049
Hemanth sai <hemanthsai0808@gmail.com>
2022-12-02 23:02:19 +0530

    Updates

---
+++ precache-manifest.2e4f6e0bf9114c59789765cbf6030819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e00f2a5c2c9906d942a",
+    "url": "/TextManuplator_using_react/static/css/main.9fcc4f8b.chunk.css"
+  },
+  {
+    "revision": "8e00f2a5c2c9906d942a",
+    "url": "/TextManuplator_using_react/static/js/main.8e00f2a5.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/TextManuplator_using_react/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "ca0340d3129ab30e3e1b",
+    "url": "/TextManuplator_using_react/static/js/2.ca0340d3.chunk.js"
+  },
+  {
+    "revision": "c1d238151d209a208ffe",
+    "url": "/TextManuplator_using_react/static/js/runtime~main.c1d23815.js"
+  },
+  {
+    "revision": "74682d70921ff8e3c0d0951889898b6d",
+    "url": "/TextManuplator_using_react/index.html"
+  }
+];
